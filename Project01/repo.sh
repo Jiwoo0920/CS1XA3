0aa1970e3a5cafc9e4b385ee2047173ba757274b
Jiwoo0920 <leej229@mcmaster.ca>
2019-02-18 22:32:56 +0000

    finished 5.1, 5.2, 5.4, 5.5, work on 5.3

--- Project01/repo.sh
+++ Project01/repo.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 cd ..
 echo "Please read README.md for description of the features"
-printf "Please select a feature:\n1:TODO Log\n2:Merge Log\n3:File Type Count\n4:Compile Error Log\n5:(Custom) chmod converter \nFeature:"
+printf "Please select a feature:\n1:TODO Log\n2:Merge Log\n3:Another Feature\n4:Compile Error Log\n5:(Custom) chmod converter \nFeature:"
 read input
+# Part 1: Interactive script (5pt) + TODO Log (5 pt)
+# Part 2: Merge Log (5 pt) + [BONUS: File Type Count (5pt) + Compile Error Log (10pt)]
 
-#1) TODO Log----------------------------------------------------------------------------------------------
+#1) TODO Log--------------------------------------------------------------------------------------------------------------------------------------DONE
 if [ $input = "1" ]; then
     #Create necessary dir/file; if exist already delete and create again
     if [ -f ~/CS1XA3/Project01/logs/todo.log ]; then
@@ -18,14 +20,12 @@ if [ $input = "1" ]; then
    #grep lines with #TODO and copy it to todo.log
    grep -rh "#TODO" > ~/CS1XA3/Project01/logs/todo.log
 
-
-
-#2) Merge Log---------------------------------------------------------------------------------------------------
+#2) Merge Log-------------------------------------------------------------------------------------------------------------------------------------DONE
 elif [ $input = "2" ]; then
     if [ -f ~/CS1XA3/Project01/logs/merge.log ]; then
         rm ~/CS1XA3/Project01/logs/merge.log
         touch ~/CS1XA3/Project01/logs/merge.log
-    else
+    else 
         touch ~/CS1XA3/Project01/logs/merge.log
     fi
 
@@ -35,14 +35,12 @@ elif [ $input = "2" ]; then
     grep -i "merge" $tmpFile1 > $tmpFile2
     cut -d " " -f 1 $tmpFile2 >> ~/CS1XA3/Project01/logs/merge.log
 
-
-
-#3) File Type Count---------------------------------------------------------------------------------------------------
+#3) File Type Count-------------------------------------------------------------------------------------------------------------------------------DONE
 elif [ $input = "3" ]; then 
     result=()
     #HTML----------------------------------------------------------------------------------------
     html=$(mktemp)
-    find ~/CS1XA3/Project01 -iname "*.html" -type f -print0 | while IFS= read -d $'\0' files; do
+    find ~/CS1XA3 -iname "*.html" -type f -print0 | while IFS= read -d $'\0' files; do
         echo $files >> $html
         done
     if [ -s $html ]; then 
@@ -53,7 +51,7 @@ elif [ $input = "3" ]; then
     fi 
     #Javascript------------------------------------------------------------------------------------
     javascript=$(mktemp)
-    find ~/CS1XA3/Project01 -iname "*.js" -type f -print0 | while IFS= read -d $'\0' files; do
+    find ~/CS1XA3 -iname "*.js" -type f -print0 | while IFS= read -d $'\0' files; do
         echo $files >> $javascript
         done 
     if [ -s $javascript ]; then
@@ -64,7 +62,7 @@ elif [ $input = "3" ]; then
     fi
     #css------------------------------------------------------------------------------------
     css=$(mktemp)
-    find ~/CS1XA3/Project01 -iname "*.css" -type f -print0 | while IFS= read -d $'\0' files; do
+    find ~/CS1XA3 -iname "*.css" -type f -print0 | while IFS= read -d $'\0' files; do
         echo $files >> $css  
         done
     if [ -s $css ]; then
@@ -75,7 +73,7 @@ elif [ $input = "3" ]; then
     fi 
     #python------------------------------------------------------------------------------------
     python=$(mktemp)
-    find ~/CS1XA3/Project01 -iname "*.py" -type f -print0 | while IFS= read -d $'\0' files; do
+    find ~/CS1XA3 -iname "*.py" -type f -print0 | while IFS= read -d $'\0' files; do
         echo $files >> $python
         done
     if [ -s $python ]; then
@@ -86,7 +84,7 @@ elif [ $input = "3" ]; then
     fi
     #haskell------------------------------------------------------------------------------------
     haskell=$(mktemp)
-    find ~/CS1XA3/Project01 -iname "*.hs" -type f -print0 | while IFS= read -d $'\0' files; do
+    find ~/CS1XA3 -iname "*.hs" -type f -print0 | while IFS= read -d $'\0' files; do
         echo $files >> $haskell
         done
     if [ -s $haskell ]; then
@@ -97,7 +95,7 @@ elif [ $input = "3" ]; then
     fi     
     #bash------------------------------------------------------------------------------------
     bash=$(mktemp)
-    find ~/CS1XA3/Project01 -iname "*.sh" -type f -print0 | while IFS= read -d $'\0' files; do
+    find ~/CS1XA3 -iname "*.sh" -type f -print0 | while IFS= read -d $'\0' files; do
         echo $files >> $bash 
         done
     if [ -s $bash ]; then
@@ -111,8 +109,9 @@ elif [ $input = "3" ]; then
 
 
 
-#4) Compile Error Log---------------------------------------------------------------------------------------------
-elif [ $input = "4" ]; then
+
+#4) Compile Error Log--------------------------------------------------------------------------------------------------------------------------------
+elif [ $input = "2" ]; then
     #Create compile_fail.log
     if [ -f ~/CS1XA3/Project01/logs/compile_fail.log ]; then 
         rm ~/CS1XA3/Project01/logs/compile_fail.log
@@ -122,7 +121,7 @@ elif [ $input = "4" ]; then
     fi
 
     #Find python files that failed to compile
-    find ~/CS1XA3/Project01 -iname "*.py" -type f -print0 | while IFS= read -d $'\0' files; do 
+    find ~/CS1XA3 -iname "*.py" -type f -print0 | while IFS= read -d $'\0' files; do 
         python -B -m py_compile $files
         if [ $? -ne 0 ]; then
             echo $files >> ~/CS1XA3/Project01/logs/compile_fail.log 
@@ -131,18 +130,9 @@ elif [ $input = "4" ]; then
         fi
         done
 
-    #Find haskell files that failed to compile
-    find ~/CS1XA3/Project01 -iname "*.hs" -type f -print0 | while IFS= read -d $'\0' files; do
-        ghc -o $files
-        if [ $? -ne 0 ]; then
-            echo $files >> ~/CS1XA3/Project01/logs/compile_fail.log
-        fi
-        done
 
-
-
-#5) (Custom) chmod converter------------------------------------------------------------------------------
-elif [ $input = "5" ]; then
+#5) (Custom) chmod converter----------------------------------------------------------------------------------------------------------------------DONE
+elif [ $input = "3" ]; then
 
     printf "Path of the directory:"
     read dirPath
