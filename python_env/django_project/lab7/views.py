7e831f807e254e972650f411ec604a865c754796
Jiwoo0920 <leej229@mcmaster.ca>
2019-03-15 20:14:45 +0000

    lab7 submission

--- python_env/django_project/lab7/views.py
+++ python_env/django_project/lab7/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def isValid(request):
-    user = request.POST.get("user","")
-    password = request.POST.get("password","")
-    if user == "Jimmy" and password == "Hendrix":
-        return HttpResponse("Cool")
-    return HttpResponse("Bad User Name")
-
 #def isValid(request):
 #    user = request.POST.get("user","")
 #    password = request.POST.get("password","")
-#    passwordAgain = request.POST.get("passwordAgain", "")
-#    if user == "Jimmy" and password == "Hendrix" and passwordAgain == "Hendrix":
+#    if user == "Jimmy" and password == "Hendrix":
 #        return HttpResponse("Cool")
 #    return HttpResponse("Bad User Name")
+
+def isValid(request):
+    user = request.POST.get("user","")
+    password = request.POST.get("password","")
+    passwordAgain = request.POST.get("passwordAgain", "")
+    if user == "Jimmy" and password == "Hendrix" and passwordAgain == "Hendrix":
+        return HttpResponse("Cool")
+    return HttpResponse("Bad User Name")
 
 #    return HttpResp
