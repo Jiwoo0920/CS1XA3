c74ee6c5e59ed3c414be0e42702f3f2d5130b859
Jiwoo0920 <leej229@mcmaster.ca>
2019-04-09 20:34:01 +0000

    edited urls.py and views.py in /webapp

--- Project03/django_project/webapp/urls.py
+++ Project03/django_project/webapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("webapp/", views.user_highscore, name="webapp_bug"),
+    path('gethighscore/', views.get_highscore, name='webapp-get_highscore'),
+    path('viewhighscore/', views.view_highscore, name='webapp-view_highscore'),
+    path('signup/', views.sign_up, name='webapp-sign_up'),
+    path('loginuser/', views.login_user, name='webapp-login_user'),
+    path('logoutuser/',views.logout_user, name='webapp-logout_user'),
 ]
--- Project03/django_project/webapp/views.py
+++ Project03/django_project/webapp/views.py
@@ -1,9 +1,59 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 import json
 from . models import UserInfo
 
 # Create your views here.
+#def session_incr(request):
+#	i = request.session.get('counter',0)
+#	request.session['counter'] = i+1
+#	return HttpResponse("Counter = " + str(request.session['counter']))
 
+#def session_get(request):
+#	return HttpResponse("Counter = " + str(request.session['counter']))
+
+
+def get_highscore(request):
+	json_req = json.loads(request.body) #takes the request and turn to dictionary 
+	username = json_req.get('username','')
+	highscore = json_req.get('highscore',0)	
+	user = User.objects.get(username=username)
+	userinfo = UserInfo.objects.get(user=user)
+	if highscore > userinfo.highscore:
+		userinfo.highscore = highscore
+	userinfo.save()
+
+def view_highscore(request):
+	reqDict = json.loads(request.body)
+	username = reqDict.get('username','')
+	user = User.objects.get(username=username)
+	userinfo = UserInfo.objects.get(user=user)
+	userhighscore = userinfo.highscore
+
+def sign_up(request):
+	json_req = json.loads(request.body)
+	uname = json_req.get('username','')
+	passw = json_req.get('password','')
+	if uname != '':
+		user = User.objects.create_user(username=uname, password=passw)
+		login(request,user)
+		return HttpResponse('SignupSuccess')
+	else:
+		return HttpResponse('SignupFail')
+
+def login_user(request):
+	json_req = json.loads(request.body)
+	uname = json_req.get('username','')
+	passw = json_req.get('password','')
+	user = authenticate(request,username=uname,password=passw)
+	if user is not None:
+		login(request,user)
+		return HttpResponse('LoggedIn')
+	else:
+		return HttpResponse('LoginFailed')
+
+def logout_user(request):
+	logout(request)
+	return HttpResponse('LoggedOut')
